bcb4fd6c29be396d0698a80efe33105c7e1c8d14
Matthewya2 <Matthewya2@gmail.com>
2016-09-12 13:07:40 -0700

    Signed-off-by: Matthewya2 <Matthewya2@gmail.com>

---
+++ Class/CPPTemplate/main.cpp
@@ -0,0 +1,31 @@
+/* 
+ * File:   main.cpp
+ * Author: Matthew
+ * Created on July 19, 2016, 9:07 AM
+ * Purpose:  Hello World Template
+ */
+
+//System Libraries Here
+#include <iostream>
+using namespace std;
+
+//User Libraries Here
+
+//Global Constants Here
+
+//Function Prototypes Here
+
+//Program Execution Begins Here
+int main(int argc, char** argv) {
+    //Declare all Variables Here
+    
+    //Input or initialize values Here
+    
+    //Process-Calculations Here
+    
+    //Output Here
+
+    //Exit
+    return 0;
+}
+
