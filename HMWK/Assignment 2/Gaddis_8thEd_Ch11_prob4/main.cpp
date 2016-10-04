19bfe23e3d4b5394d994b73be2f3cf7a98a07542
rcc <rcc>
2016-10-04 14:10:38 -0700

    Signed-off-by: rcc <rcc>

--- HMWK/Assignment 2/Gaddis_8thEd_Ch11_prob4/main.cpp
+++ HMWK/Assignment 2/Gaddis_8thEd_Ch11_prob4/main.cpp
@@ -2,7 +2,17 @@
  * File:   main.cpp
  * Author: Dr. Mark E. Lehr
  * Created on July 19, 2016, 9:07 AM
- * Purpose:  Hello World Template
+ * Purpose:  The program should have an array of 12 structures 
+ * to hold weather data for an entire year. When the program runs,
+ *  it should ask the user to enter data for each month. 
+ * (The average temperature should be calculated.) Once the data 
+ * are entered for all the months, the program should calculate 
+ * and display the average monthly rainfall, the total rainfall 
+ * for the year, the highest and lowest temperatures for the year 
+ * (and the months they occurred in), and the average of all the 
+ * monthly average temperatures.
+Input Validation: Only accept temperatures within the range 
+ * between –100 and +140 degrees Fahrenheit.
  */
 
 //System Libraries Here
@@ -16,121 +26,98 @@ using namespace std;
 //Like PI, e, Gravity, or conversions
 
 //Function Prototypes Here
-struct Sales{
-    float totalR;
+struct Weather{
+    float totalMR;
     float tempH;
     float tempL;
     float avrgTmp;
 };
 
-void print(Sales);
 //Program Execution Begins Here
 int main(int argc, char** argv) {
-    Sales north;
-    Sales east;
-    Sales south;
-    Sales west;
-    short count=0;  //counter
+    const int MONTHS=12;
+    Weather weather[MONTHS];
+    short i;  //counter
+    float totalYR=0;        //total yearly rain
+    float avgMR;            //average monthly rain
+    float avgAvgM=0;        //average of the average monthly temperature
+    int monthH, monthL; //month where the highest temp is and lowest temp
+    int lowT;           //highest temp
+    int highT;          //lowest temp
+    
+    for(i=0;i<MONTHS;i++){
+    cout << "What is the total rain for month "<< i+1 <<"?"<<endl;
+    cin >> weather[i].totalMR;
     
     do{
-    if (count>0){cout << "please input numbers that arent negative,"
-            "into these values"<<endl;}    
-    cout << "North Division: "<<endl;
-    north.name="North";
-    cout << "What are the first quarter sales?"<<endl;
-    cin >> north.first;
-    cout <<"What are the second quarter sales?"<<endl;
-    cin >> north.second;
-    cout <<"What are the third quarter sales?"<<endl;
-    cin >> north.third;
-    cout <<"What are the fourth quarter sales?"<<endl;
-    cin >>north.fourth;
-    
-    count++;
-    }while(north.first<0 or north.second<0 or north.third<0 or north.fourth<0);
-    
-    north.total=north.first+north.second+north.third+north.fourth;
-    north.average=north.total/4;
-    count=0;
+    cout << "What is the highest temperature this month in fahrenheit?"<<endl;
+    cin >> weather[i].tempH;
+    //input validation
+    if(weather[i].tempH<=(-100) || weather[i].tempH>=140){
+        cout << "Please use temperatures higher than -100 and less than"
+                " 140."<<endl;
+    }
+    }while(weather[i].tempH<=(-100) || weather[i].tempH>=140);
     
     do{
-    if (count>0){cout << "please input numbers that arent negative,"
-            "into these values"<<endl;}
-    cout << "East Division: "<<endl;
-    east.name="East";
-    cout << "What are the first quarter sales?"<<endl;
-    cin >> east.first;
-    cout <<"What are the second quarter sales?"<<endl;
-    cin >> east.second;
-    cout <<"What are the third quarter sales?"<<endl;
-    cin >> east.third;
-    cout <<"What are the fourth quarter sales?"<<endl;
-    cin >>east.fourth;
-    count++;
-    }while(east.first<0 or east.second<0 or east.third<0 or east.fourth<0);
+    cout <<"What is the lowest temperature this month"
+            "in fahrenheit?"<<endl;
+    cin >> weather[i].tempL;  
+    //input validation
+    if(weather[i].tempH<=(-100) || weather[i].tempH>=140){
+        cout << "Please use temperatures higher than -100 and less than"
+                " 140."<<endl;
+    }
     
-    east.total=east.first+east.second+east.third+east.fourth;
-    east.average=east.total/4;
-    count=0;
+    }while(weather[i].tempL<=(-100) || weather[i].tempL>=140);
+    }
     
-    do{
-    if (count>0){cout << "please input numbers that arent negative,"
-            "into these values"<<endl;}
-    cout << "South Division: "<<endl;
-    south.name="South";
-    cout << "What are the first quarter sales?"<<endl;
-    cin >> south.first;
-    cout <<"What are the second quarter sales?"<<endl;
-    cin >> south.second;
-    cout <<"What are the third quarter sales?"<<endl;
-    cin >> south.third;
-    cout <<"What are the fourth quarter sales?"<<endl;
-    cin >>south.fourth;
-    count++;
-    }while(south.first<0 or south.second<0 or south.third<0 or south.fourth<0);    
+    for(i=0;i<MONTHS;i++){        //average temperature of the month
+    weather[i].avrgTmp=(weather[i].tempL+weather[i].tempH) / 2;
+    }
     
-    south.total=south.first+south.second+south.third+south.fourth;
-    south.average=south.total/4;
-    count=0;
+    for(i=0;i<MONTHS;i++){
+        totalYR+=weather[i].totalMR; 
+    }
     
-    do{
-    if (count>0){cout << "please input numbers that arent negative,"
-            "into these values"<<endl;}
-    cout << "West Division: "<<endl;
-    west.name="West";
-    cout << "What are the first quarter sales?"<<endl;
-    cin >> west.first;
-    cout <<"What are the second quarter sales?"<<endl;
-    cin >> west.second;
-    cout <<"What are the third quarter sales?"<<endl;
-    cin >> west.third;
-    cout <<"What are the fourth quarter sales?"<<endl;
-    cin >>west.fourth;
-    count++;
-    }while(west.first<0 or west.second<0 or west.third<0 or west.fourth<0);    
+    for(i=0;i<MONTHS-1;i++){       
+        if(weather[i].tempL<weather[i+1].tempL){
+            lowT=weather[i].tempL;
+            monthL=i;
+        }
+        if (weather[i].tempL>weather[i+1].tempL){
+            lowT=weather[i+1].tempL;
+            monthL=i+1;
+        }
+        
+        if(weather[i].tempH>weather[i+1].tempH){
+            highT=weather[i].tempH;
+            monthH=i;
+        }
+        if(weather[i].tempH<weather[i+1].tempH){
+            highT=weather[i+1].tempH;
+            monthH=i+1;
+        }
+    }
     
-    west.total=west.first+west.second+west.third+west.fourth;
-    west.average=west.total/4;
-    count=0;
-   
-    print(north);
+    for(i=0;i<MONTHS;i++){
+        avgAvgM+=weather[i].avrgTmp;        
+    }
     
-    print(east);
+    avgAvgM/=MONTHS;
+    avgMR=totalYR/MONTHS;
     
-    print(south);
-    
-    print(west);
-    //Exit
-    return 0;
-}
-
-void print( Sales div ){
     
     cout <<endl;
     cout <<"~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"<<endl;
-    cout <<div.name<<" division."<<endl;
-    cout <<"Earned: $"<<div.total<<" total."<<endl;
-    cout <<"And has an average quarter sale of "<<div.average<<endl;
+    cout <<"the average monthly rainfall is "<< avgMR <<"."<<endl;
+    cout <<"The total yearly rainfall is "<< totalYR <<"."<<endl;
+    cout <<"the highest temperature was "<< monthH <<", and that occurred"
+            "in month #"<< monthH <<","<<endl;
+    cout << "while the lowest was "<< monthL <<", and that occurred in month"
+                    " #"<<monthL<<endl;
+    cout << "the monthly average temperature is "<<avgAvgM<<".";
     
-    
+    //Exit
+    return 0;
 }
